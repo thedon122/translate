9b623f46666cbcb1107ce888c982f3c2c28ff574
thedon122 <donovan122@yahoo.com>
2022-03-23 20:54:00 +0100

    added button created context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,14 @@
 import React from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends React.Component {
+    static contextType = LanguageContext; 
+    
     render() {
         return <button className="ui button primary">Submit</button>
     }
 }
+
+<Button />
 
 export default Button;
