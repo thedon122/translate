ad68539687af882f6b405e541039d1e9fadcd516
thedon122 <donovan122@yahoo.com>
2022-03-25 17:42:00 +0100

    add funtionality to flags

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,8 @@ class Button extends React.Component {
     static contextType = LanguageContext; 
     
     render() {
-        return <button className="ui button primary">Submit</button>
+        const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+        return <button className="ui button primary">{text}</button>
     }
 }
 
