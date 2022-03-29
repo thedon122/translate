c233aa003353797270dbcc96fb2e6c410b9ed2e5
thedon122 <donovan122@yahoo.com>
2022-03-29 19:04:00 +0100

    modifty appjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,22 @@
-import React from "react";
-import UserCreate from "./UserCreate";
-import LanguageContext from "../contexts/LanguageContext";
-import ColorContext from "../contexts/ColorContext";
-import LanguageSelector from "./LanguageSelector";
+import React from 'react';
+import UserCreate from './UserCreate';
+import { LanguageStore } from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-    state = { language: 'english' };
-
-    onLanguageChange = language => {
-        this.setState({ language: language });
-    };
-    render() {
-        return(<div className="ui container">
-            <LanguageSelector onLanguageChange={this.onLanguageChange} />
-            <ColorContext.Provider value="red">
-                <LanguageContext.Provider value={this.state.language}>
-                    <UserCreate />
-                </LanguageContext.Provider>
-            </ColorContext.Provider>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="red">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -5,8 +5,8 @@ class LanguageSelector extends React.Component {
         return (
             <div>
                 Select a language:
-                <i className="flag us" onClick={() => this.props.onLanguageChange('english')}/>
-                <i className="flag nl" onClick={() => this.props.onLanguageChange('dutch')} />
+                <i className="flag us" onClick={() => this.context.onLanguageChange('english')}/>
+                <i className="flag nl" onClick={() => this.context.onLanguageChange('dutch')} />
             </div>
         );
     }
