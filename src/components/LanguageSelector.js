f376128e3aa151e478ae5f2dc5c774d4350ce89d
thedon122 <donovan122@yahoo.com>
2022-03-29 17:23:00 +0100

    connected button and field to flag funtionalty

--- src/components/LanguageSelector.js
+++ src/components/LanguageSelector.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class LanguageSelector extends React.Component {
+    static contextType = LanguageContext;
+
     render () {
+        console.log(this.context);
         return (
             <div>
                 Select a language:
