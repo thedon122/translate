0eeb621116ed7463311a72f1dfb16911fc82ca3e
thedon122 <donovan122@yahoo.com>
2022-03-29 19:53:00 +0100

    added context functionalty to onLanguageContext

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
+import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
     state = { language: 'english' };
@@ -10,14 +12,12 @@ class App extends React.Component {
     };
     render() {
         return(<div className="ui container">
-            <div>
-                Select a language:
-                <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
-                <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
-            </div>
-            <LanguageContext.Provider value={this.state.language}>
-                <UserCreate />
-            </LanguageContext.Provider>
+            <LanguageSelector onLanguageChange={this.onLanguageChange} />
+            <ColorContext.Provider value="red">
+                <LanguageContext.Provider value={this.state.language}>
+                    <UserCreate />
+                </LanguageContext.Provider>
+            </ColorContext.Provider>
         </div>
         );
     }
